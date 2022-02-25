b30a1a471111765ae65b0132d3fcd3c7dc67faf8
João Junior <joaodcpjunior@gmail.com>
2022-02-25 11:29:15 -0300

    bootstrap project

---
+++ src/main/java/com/joaodcpjunior/servicefilesystemwatcher/ServiceFileSystemWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.joaodcpjunior.servicefilesystemwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFileSystemWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFileSystemWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaodcpjunior/servicefilesystemwatcher/ServiceFileSystemWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodcpjunior.servicefilesystemwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFileSystemWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
